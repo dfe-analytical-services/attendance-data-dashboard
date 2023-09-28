6966974c47d4f9ec0f8f87d2ee106da1637ad2ce
SELBY <Gemma.SELBY@education.gov.uk>
2023-09-28 09:01:21 +0100

    Addition of data for first full week of AY 23/24 - w/c 11th September

--- global.R
+++ global.R
@@ -104,33 +104,34 @@ site_c <- ""
 # Data manipulation ----------------------------------------------------------------------------
 # Read in data
 # attendance_data_raw <- fread("data/Weekly_dummy_data.csv")
-start_date <- as.Date("2022-09-12")
-end_date <- as.Date("2023-07-28")
-funeral_date <- as.Date("2022-09-19")
-strike_date_1 <- as.Date("2023-02-01")
-strike_date_2 <- as.Date("2023-03-15")
-strike_date_3 <- as.Date("2023-03-16")
-strike_date_4 <- as.Date("2023-04-27")
-strike_date_5 <- as.Date("2023-05-02")
-strike_date_6 <- as.Date("2023-07-05")
-strike_date_7 <- as.Date("2023-07-07")
+start_date <- as.Date("2023-09-11")
+end_date <- as.Date("2023-09-15")
+# funeral_date <- as.Date("2022-09-19")
+# strike_date_1 <- as.Date("2023-02-01")
+# strike_date_2 <- as.Date("2023-03-15")
+# strike_date_3 <- as.Date("2023-03-16")
+# strike_date_4 <- as.Date("2023-04-27")
+# strike_date_5 <- as.Date("2023-05-02")
+# strike_date_6 <- as.Date("2023-07-05")
+# strike_date_7 <- as.Date("2023-07-07")
 
-regional_strike_1 <- as.Date("2023-02-28")
-regional_strike_2 <- as.Date("2023-03-01")
-regional_strike_3 <- as.Date("2023-03-02")
-autumn_start <- as.Date("2022-09-12")
-autumn_end <- as.Date("2022-12-16")
-spring_start <- as.Date("2023-01-03")
-spring_end <- as.Date("2023-03-31")
-summer_start <- as.Date("2023-04-01")
-summer_end <- as.Date("2023-07-21")
+# regional_strike_1 <- as.Date("2023-02-28")
+# regional_strike_2 <- as.Date("2023-03-01")
+# regional_strike_3 <- as.Date("2023-03-02")
+# autumn_start <- as.Date("2022-09-12")
+# autumn_end <- as.Date("2022-12-16")
+# spring_start <- as.Date("2023-01-03")
+# spring_end <- as.Date("2023-03-31")
+# summer_start <- as.Date("2023-04-01")
+# summer_end <- as.Date("2023-07-21")
 
 # most_recent_week_dates <- paste("Most recent week -", as.Date(end_date)-4, "to", as.Date(end_date))
-most_recent_week_dates <- paste("Latest week -", as.Date(end_date) - 11, "to", as.Date(end_date) - 7)
+# most_recent_week_dates <- paste("Latest week -", as.Date(end_date) - 11, "to", as.Date(end_date) - 7)
+most_recent_week_dates <- paste("Latest week -", as.Date(start_date), "to", as.Date(end_date))
 # ytd_dates <- paste("Year to date -", as.Date(start_date), "to", as.Date(end_date))
-ytd_dates <- paste("Year to date -", as.Date(start_date), "to", as.Date(end_date) - 7)
+ytd_dates <- paste("Year to date -", as.Date(start_date), "to", as.Date(end_date))
 
-school_freq_count <- fread("data/enrolments_schools_denominator_310723split.csv")
+school_freq_count <- fread("data/enrolments_schools_denominator_180923.csv")
 school_freq_count$total_enrolments <- as.numeric(school_freq_count$total_enrolments)
 
 attendance_data <- read.csv("data/attendance_data_dashboard.csv")
@@ -158,6 +159,7 @@ school_type_lookup <- attendance_data %>%
 la_list <- geog_lookup %>%
   dplyr::select(region_name, la_name) %>%
   filter(region_name != "All") %>%
+  filter(la_name != "NA") %>%
   distinct() %>%
   arrange(region_name, la_name) %>%
   group_by(region_name) %>%
