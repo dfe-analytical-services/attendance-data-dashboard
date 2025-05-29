d809e6035cc78d4d110e9f5e152ccc4f8955e5a6
Rich Bielby <richbielby@e.email>
2025-05-29 09:19:23 +0100

    Fixing a11y panel location

--- ui.R
+++ ui.R
@@ -94,15 +94,19 @@ ui <- function(input, output, session) {
           )
         )
       ),
-      a11y_panel(
-        dashboard_title = site_title,
-        dashboard_url = site_primary,
-        date_tested = "26/11/2023",
-        date_prepared = "27/11/2023",
-        date_reviewed = "27/11/2023",
-        issues_contact = team_email,
-        publication_name = "Pupil attendance in schools in England",
-        publication_slug = "pupil-attendance-in-schools"
+      shiny::tabPanel(
+        value = "a11y_panel",
+        "Accessibility",
+        a11y_panel(
+          dashboard_title = site_title,
+          dashboard_url = site_primary,
+          date_tested = "26/11/2023",
+          date_prepared = "27/11/2023",
+          date_reviewed = "27/11/2023",
+          issues_contact = team_email,
+          publication_name = "Pupil attendance in schools in England",
+          publication_slug = "pupil-attendance-in-schools"
+        )
       )
     ),
     gov_layout(
